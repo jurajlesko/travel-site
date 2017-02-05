cc915531b2a6d3ba556fde43f6c7bd2d7b825f13
jurajlesko <jurlesko@gmail.com>
2017-02-05 18:01:35 +0100

    Completed the reveal on scroll behavior

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,14 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobilMenu = new MobileMenu();
 
-
-
-
-
-
-
-
+new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.testimonial'), '60%');
 
 
 
